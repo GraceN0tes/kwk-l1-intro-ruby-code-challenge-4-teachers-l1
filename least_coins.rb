5705f722fc79dc5cec13499f7807fa5c0bed3cfe
Grace Park <parkgra3@gmail.com>
2018-08-09 17:41:16 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,30 +1,30 @@
 def least_coins(cents)
   coin_types = {
-    :quarters=>0,
-    :dimes=>0,
-    :nickels=>0,
-    :pennies=>0
+    :quarters=>0.25,
+    :dimes=>0.10,
+    :nickels=>0.05,
+    :pennies=>0.01
   }
 
-  quarters = cents/25
-  if quarters == 0
+  quarters = cents/0.25
+  if quarters == 0.25
     coin_types[:quarters] = 0
-  elsif quarters != 0
+  elsif quarters != .25
     coin_types[:quarters] = quarters
-    cents = cents - quarters*25
+    cents = cents - quarters*.25
   end
 
-  dimes = cents/10
-  if dimes == 0
-    coin_types[:dimes] = 0
-  elsif dimes != 0
+  dimes = cents/.10
+  if dimes == .10
+    coin_types[:dimes] = .10
+  elsif dimes != .10
     coin_types[:dimes] = dimes
-    cents = cents - dimes*10
+    cents = cents - dimes*.10
   end
 
-  nickels = cents/5
+  nickels = cents/.05
   if nickels == 0
-    coin_types[:nickels] = 0
+    coin_types[:nickels] = .05
   elsif nickels != 0
     coin_types[:nickels] = nickels
     cents = cents - nickels*5
